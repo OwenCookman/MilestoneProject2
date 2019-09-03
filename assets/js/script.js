51b32069d6fd0ee2c9b22ace2efae3342e106455
OwenCookman <ozzycookman@hotmail.com>
2019-09-03 23:11:46 +0100

    Added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ let card = document.getElementsByClassName("Card");
 let cards = [...card];
 
 for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", displayCard);
+    cards[i].addEventListener("click", function(){
+        this.classList.add("flip-card")
+    });
     }
 
