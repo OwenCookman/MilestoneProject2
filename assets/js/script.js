3b3281aa74ab60f90dbf963ebbb78b045ae90c6e
OwenCookman <ozzycookman@hotmail.com>
2019-09-03 23:08:24 +0100

    Added card array and event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
-document.getElementById("cardEvent").addEventListener("click", function () {
-    this.classList.add(".flip-card");
-}
+let card = document.getElementsByClassName("Card");
+let cards = [...card];
+
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", displayCard);
+    }
+
