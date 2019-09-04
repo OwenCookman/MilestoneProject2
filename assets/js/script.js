e16dc55024c4d13d28ff6db52aca414679db2d12
OwenCookman <ozzycookman@hotmail.com>
2019-09-04 22:24:19 +0100

    Removed unnecessary <section> tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ let card = document.getElementsByClassName("Card");
 let cards = [...card];
 
 for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", function(){
+    cards[i].addEventListener("click", function () {
         this.classList.toggle("flip-card")
     });
-    }
+}
 
