45a931d15ab7e4d772cdba87f7d2fbfb5d6a9386
OwenCookman <ozzycookman@hotmail.com>
2019-09-04 21:31:49 +0100

    Implemented event listener & loop for cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+let card = document.getElementsByClassName("Card");
+let cards = [...card];
+
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function(){
+        this.classList.add(".flip-card")
+    });
+    }
 
