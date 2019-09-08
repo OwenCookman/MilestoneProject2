e0fab90bf4e911ef8910b50df65c777c2d648598
OwenCookman <ozzycookman@hotmail.com>
2019-09-08 21:53:38 +0100

    Added flip animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let cards = [...card];
 for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", function () {
         this.classList.toggle("flip-card")
+        this.classList.toggle("flip")
     });
 }
 
