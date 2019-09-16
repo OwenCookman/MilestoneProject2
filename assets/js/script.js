1287bdabb4c1bed1c7aef3528c07185ea6ddc026
OwenCookman <ozzycookman@hotmail.com>
2019-09-16 22:10:22 +0100

    Added start of flippedCards if/else

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var flippedCards = [];
-    $("ul>li").click(function() {
+    
+    $("ul>li").click(function () {
         $(this).toggleClass("flip");
         $(this).toggleClass("disable");
+        (flippedCards).push(this.type);
+        console.log(flippedCards);
     })
-    
+
+    if (flippedCards.length == 2) {
+        if (flippedCards[0] == flippedCards[1]) {
+            $(flippedCards).addClass("matched");
+            $(flippedCards).removeClass("flip");
+        } else {
+            $(flippedCards).addClass("not-match");
+        }
+        var flippedCards = [];
+    }
 })
+
+
 
