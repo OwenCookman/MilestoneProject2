e160f3f6db22735167ef7fdfc58530b70bce30f9
OwenCookman <ozzycookman@hotmail.com>
2019-09-17 22:38:47 +0100

    Added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,35 @@
 $(document).ready(function () {
+    //Global Variables
     var flippedCards = [];
     var cards = document.querySelector("ul");
 
+    /**
+     * Credit for this function goes to user Alexey Lebedev on Stack Overflow
+     */
     function startGame() {
         for (var i = cards.children.length; i >= 0; i--) {
             cards.appendChild(cards.children[Math.random() * i | 0]);
         }
     }
 
+    /**
+     * targets the HTML element with ID of restart 
+     * When this element is clicked it runs the startGame() function
+     * and removes any matched and disable classes from any elements with the class card
+     * and sets the move counter back to 0
+     */
     $("#restart").click(function() { 
         startGame();
         $(".card").removeClass("matched");
         $(".card").removeClass("disable");
+        $("#moves")[0].innerHTML = 0;
     })
 
+    /**
+     * Targets all <li> that are children of <ul> elements
+     * when they are clicked apply the classes flip and disable
+     * Push the element to the flippedCards array and run the checkMatch() function
+     */
     $("ul>li").click(function () {
         $(this).toggleClass("flip");
         $(this).toggleClass("disable");
@@ -21,10 +37,25 @@ $(document).ready(function () {
         checkMatch(flippedCards);
     })
 
+    /**
+     * targets element with ID moves
+     * when called this function increases the innerHTML value by 1
+     */
     function moved() {
-        $(".moves").innerHTML ++;
+        $("#moves")[0].innerHTML ++;
     }
 
+    /**
+     * When called this function will check if the length of the flippedCards array is 2
+     * If this return true it will call the moved() function to add 1 to the move counter
+     * It then checks the objects types in the flippedCards array
+     * If they match it will add the class matched and remove flip
+     * If they dont match it will add the not-match class and remove the flip class
+     * As well as adding the disable class to all elements with the card class
+     * it will then wait 1100 miliseconds before removing not-match and disable from all cards
+     * Then reapply disable to any cards with the class matched
+     * Finally it will reset the flippedCards length to 0
+     */
     function checkMatch(flippedCards) {
         if (flippedCards.length == 2) {
             moved();
